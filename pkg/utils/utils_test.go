e19715ba3c5cd0a2b0965b46a81a51d7e69835e4
Zulh <75463191+zulh-civo@users.noreply.github.com>
2021-07-26 14:58:34 +0800

    Bugfix: terminating app(s) shouldn't block new app(s) from being reconciled - take 2 (#7)

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -291,7 +291,7 @@ func TestIsStrSliceContains(t *testing.T) {
 
 func TestGetAppDependencies1(t *testing.T) {
 	actual, _ := GetAppDependencies("mariadb")
-	expected := []string{"longhorn"}
+	expected := []string{}
 	if !elementsMatch(expected, actual) {
 		t.Errorf("Expected %s but got %s", expected, actual)
 	}
